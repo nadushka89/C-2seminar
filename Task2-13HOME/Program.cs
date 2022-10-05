86aeaf09f6d046ba0ce498418d587b85a267ae3b
Надюшка Рыбка <kitti-46@ya.ru>
2022-10-05 21:59:41 +0300

    added 13 home

---
+++ Task2-13HOME/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного 
+числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+void GetResult(int numeric)
+{
+    if (numeric > 99)
+    {
+        string numericAsString = numeric.ToString();
+        int result = int.Parse(numericAsString[2].ToString());
+        Console.WriteLine(result);
+    }
+    else
+        Console.WriteLine("Третьей цифры нет");
+}
+GetResult(78);
