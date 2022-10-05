0ee5cc2bceb99338b27566125cde65b50128ee15
Надюшка Рыбка <kitti-46@ya.ru>
2022-10-05 22:00:07 +0300

    added 10 home

---
+++ Task2-10HOME/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 10: Напишите программу, которая принимает 
+на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+void GetResult(int numeric)
+{
+    if (numeric < 1000 && numeric > 99)
+    {
+        string numericAsString = numeric.ToString();
+        int result = int.Parse(numericAsString[1].ToString());
+        Console.WriteLine(result);
+    }
+    else
+        Console.WriteLine("Введено не трехзначное число");
+}
+GetResult(918);
